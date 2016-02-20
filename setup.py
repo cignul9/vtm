05ab4b971e10068d9d2059ed73bf7c7e858b2bf8
Shawn Magill <Shawn Magill>
2016-02-19 23:50:31 -0700

    fixed __init__.py to import classes from vtm.py

--- setup.py
+++ setup.py
@@ -2,12 +2,26 @@ from distutils.core import setup
 setup(
   name = 'vtm',
   packages = ['vtm'],
-  version = '0.1',
+  version = '0.2',
   description = 'A library to facilitate using the API to configure Brocade Virtual Traffic Managers',
   author = 'Shawn Magill',
   author_email = 'cignul9@gmail.com',
   url = 'https://github.com/cignul9',
-  download_url = 'https://github.com/cignul9/vtm/tarball/0.1',
-  keywords = ['stingray', 'vtm', 'brocade', 'traffic manager', 'api'],
+  download_url = 'https://github.com/cignul9/vtm/tarball/0.2',
+  install_requires=[
+    'requests',
+    'json',
+    'sys',
+    'warnings',
+    'pprint',
+    're',
+  ],
+  keywords = [
+    'stingray',
+    'vtm',
+    'brocade',
+    'traffic manager',
+    'api'
+  ],
   classifiers = [],
 )
---
+++ vtm/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.2'
+__author__ = 'Shawn Magill'
+__all__ = [ 'VtmConnection', 'VtmConfig' ]
+
+from .vtm import VtmConnection, VtmConfig
