71ace9437b3dd2546f32ca394b80e974ee4664d6
cignul9 <cignul9@gmail.com>
2016-03-03 09:53:56 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'vtm',
   packages = ['vtm'],
-  version = '1.0',
+  version = '1.2',
   description = 'Facilitates using the API to configure Brocade Virtual Traffic Managers',
   author = 'Shawn Magill',
   author_email = 'cignul9@gmail.com',
   url = 'https://github.com/cignul9',
-  download_url = 'https://github.com/cignul9/vtm/tarball/1.0',
+  download_url = 'https://github.com/cignul9/vtm/tarball/1.2',
   install_requires = [
     'requests',
   ],
--- vtm/__init__.py
+++ vtm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0'
+__version__ = '1.2'
 __author__ = 'Shawn Magill'
 __all__ = [ 'VtmConnection', 'VtmConfig' ]
 
